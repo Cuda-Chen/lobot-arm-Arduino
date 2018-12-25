e6ce2a8f2424e131096b5edc782f07ff329ba434
Cuda-Chen <clh960524@gmail.com>
2018-12-25 17:26:13 +0800

    add main code

---
+++ main/main.ino
@@ -0,0 +1,21 @@
+#include <LobotServoController.h>
+
+LobotServoController myServo;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600); // Lobot board likes baud rate 9600
+  // if serial connection sucks
+  while(!Serial);
+  digitalWrite(13, HIGH);
+
+  myServo.moveServo(0, 1500, 1000);
+  delay(2000);
+}
+
+void loop() {
+  myServo.moveServo(0, 1400, 1000);
+  delay(2000);
+  myServo.moveServo(0, 1600, 1000);
+  delay(2000);
+}
